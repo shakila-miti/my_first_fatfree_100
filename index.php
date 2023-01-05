bfd4da4ee05fd9c9d8f395af1a8b2023308f57f0
shakila-miti <shakila.miti10@gmail.com>
2023-01-05 18:08:42 +0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+if (file_exists('vendor/autoload.php')) {
+	// load via composer
+	require_once('vendor/autoload.php');
+	$f3 = \Base::instance();
+} elseif (!file_exists('lib/base.php')) {
+	die('library not found`.');
+} else {
+	$f3=require('lib/base.php');
+}
+
+$f3->set('DEBUG',1);
+if ((float)PCRE_VERSION<8.0)
+	trigger_error('PCRE version is out of date');
+
+// Load configuration
+$f3->config('config.ini');
+
+$f3->route('GET /',
+    function() {
+        echo 'This is my root!!!';
+    }
+);
+
+$f3->route('GET /me',
+    function() {
+        echo 'My name is Miti!';
+    }
+);
+
+$f3->run();
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'bcosca/fatfree',
+        'pretty_version' => '3.8.1',
+        'version' => '3.8.1.0',
+        'reference' => NULL,
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'bcosca/fatfree' => array(
+            'pretty_version' => '3.8.1',
+            'version' => '3.8.1.0',
+            'reference' => NULL,
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'bcosca/fatfree-core' => array(
+            'pretty_version' => '3.8.1',
+            'version' => '3.8.1.0',
+            'reference' => 'f6ea45472f55a1ecabd4ced74cb56f0efd6af34e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../bcosca/fatfree-core',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
